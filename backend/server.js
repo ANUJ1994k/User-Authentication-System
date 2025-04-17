dc33c92b200b6ec36f9608b7998c5dda4f38df61
ANUJ KUMAR <anuj1994k>
2025-04-17 20:22:48 +0530

    *

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: 'https://user-authentication-system-5njf2glq5.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    credentials: true, // If you're using cookies
-  }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 
